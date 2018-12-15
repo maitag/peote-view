12bd3e66b60a97a0e46f6683b242019c30118070
Sylvio Sell <info@maitag.de>
2018-12-15 21:02:50 +0100

    add animation with slots and tiles to sample

--- src/TextureSlotTiles.hx
+++ src/TextureSlotTiles.hx
@@ -29,7 +29,8 @@ class Elem implements Element
 	@texUnit() public var unit:Int;
 
 	// what texture-slot to use
-	@texSlot public var slot:Int;  // unsigned 2 bytes integer
+	@texSlot  @anim("Slot") @time("Slot")
+	public var slot:Int;  // unsigned 2 bytes integer
 
 	// manual texture coordinates inside a slot (or inside all slots if no slot available)
 	//@texX public var tx:Int;
@@ -112,14 +113,23 @@ class TextureSlotTiles
 			element1.tile = 1;
 			buffer.addElement(element1);     // element to buffer
 			
-			// Animated Tile
+			// Animated Slot and Tile
 			element2  = new Elem(0, 350, 200, 200);
 			element2.unit = 1;
-			element2.slot = 0;
-			element2.animTile(0, 255);
-			element2.timeTile(0, 30);
-			buffer.addElement(element2);     // element to buffer
 			
+			var timePerSlot = 10;
+			for (i in 0...3)
+				Timer.delay(function() {
+					trace("Slot " + i);
+					element2.animTile(0, (i == 3-1) ? 31 : 32);
+					element2.timeTile(i*timePerSlot, timePerSlot);
+					buffer.updateElement(element2);
+				}, i*timePerSlot*1000);
+			
+			element2.animSlot(0, 2);
+			element2.timeSlot(0, timePerSlot*2);
+			
+			buffer.addElement(element2);     // element to buffer
 			peoteView.start();
 		}
 		catch (msg:String) {trace("ERROR", msg); }
@@ -152,10 +162,10 @@ class TextureSlotTiles
 		//switch (keyCode) {
 		// neko needs Std.int(keyCode) !!!
 		switch (Std.int(keyCode)) {
-			case KeyCode.NUMBER_1: element0.slot = (element0.slot !=0 ) ? element0.slot-1 : 3 ; buffer.updateElement(element0);
-			case KeyCode.NUMBER_2: element0.slot = (element0.slot+1) % 4; buffer.updateElement(element0);
-			case KeyCode.NUMBER_3: element1.slot = (element1.slot !=0 ) ? element1.slot-1 : 2 ; buffer.updateElement(element1);
-			case KeyCode.NUMBER_4: element1.slot = (element1.slot+1) % 3; buffer.updateElement(element1);
+			case KeyCode.NUMBER_1: element0.slot = (element0.slotStart !=0 ) ? element0.slotStart-1 : 3 ; buffer.updateElement(element0);
+			case KeyCode.NUMBER_2: element0.slot = (element0.slotStart+1) % 4; buffer.updateElement(element0);
+			case KeyCode.NUMBER_3: element1.slot = (element1.slotStart !=0 ) ? element1.slotStart-1 : 2 ; buffer.updateElement(element1);
+			case KeyCode.NUMBER_4: element1.slot = (element1.slotStart+1) % 3; buffer.updateElement(element1);
 			case KeyCode.NUMBER_5: element1.tile = (element1.tileStart !=0 ) ? element1.tileStart-1 : 31 ; buffer.updateElement(element1);
 			case KeyCode.NUMBER_6: element1.tile = (element1.tileStart+1) % 32; buffer.updateElement(element1);
 			default:
