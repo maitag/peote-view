1949407c66c8ba9d360d41b89ccd7f530d5faedb
Sylvio Sell <info@maitag.de>
2019-05-08 15:29:38 +0200

    fixing Loader and Texturecaching-Sample, testing on android, codecleaning

--- src/TextureCaching.hx
+++ src/TextureCaching.hx
@@ -54,7 +54,7 @@ class TextureCaching
 	{
 		try {
 			peoteView = new PeoteView(window.context, window.width, window.height);
-			display   = new Display(10,10, window.width-20, window.height-20, Color.GREEN);
+			display   = new Display(0,0, window.width, window.height, Color.GREEN);
 			peoteView.addDisplay(display);
 			
 			buffer  = new Buffer<Elem>(100);
@@ -67,12 +67,18 @@ class TextureCaching
 					{width:256, height:256, slots:4},
 					{width:512, height:512, slots:8},
 					{width:1024, height:1024, slots:16},
+					{width:1920, height:1280, slots:8},
 				],
 				peoteView.gl.getParameter(peoteView.gl.MAX_TEXTURE_SIZE)
 			);
 			
 			Loader.corsServer = "cors-anywhere.herokuapp.com";
 			
+			program.setMultiTexture(textureCache.textures, "custom");
+			
+/*			var progressSumA:Array<Int> = [for(i in 0...19) 0];
+			var progressSumB:Array<Int> = [for (i in 0...19) 0];
+*/			
 			Loader.imageArray([
 				"assets/images/test0.png",
 				"assets/images/test1.png",
@@ -84,34 +90,41 @@ class TextureCaching
 				"http://maitag.de/semmi/blender/mandelbulb/mandelbulb_volume_1001f.blend.png",
 				"http://maitag.de/semmi/blender/lyapunov/example_images/displace-FOSSIL-13.blend.png",
 				"https://upload.wikimedia.org/wikipedia/commons/8/80/Salvador_Dali_The_Rainbow_1972.jpg",
+				"http://maitag.de/semmi/blender/spheresfractal_07_lights.png",
+				"http://maitag.de/semmi/blender/lyapunov/example_images/displace-FOSSIL-19.blend.png",
+				"http://maitag.de/semmi/blender/lyapunov/example_images/volume-fake_07.blend.png",
+				"http://maitag.de/semmi/blender/lyapunov/example_images/microcycles/lyap-displace-test_19.blend.png",
+				"http://maitag.de/semmi/blender/lyapunov/example_images/microcycles/lyap-displace-test_07.blend.png",
+				"http://maitag.de/semmi/blender/mandelbulb/mandelverse_10.blend.jpg",
+				"http://maitag.de/semmi/blender/mandelbulb/mandelverse_11.blend.jpg",
+				"http://maitag.de/semmi/blender/mandelbulb/mandelverse_12.blend.jpg",
+				"http://maitag.de/semmi/blender/mandelbulb/mandelverse_13.blend.jpg",
 				], //true,
-				function(loaded:Int, size:Int) {
-					// TODO: name param
-					trace(Std.int(loaded/size * 100) + "%" ,' ($loaded / $size)');
-				},
-				//function(image:Image) { // after every single image is loaded
-					// TODO: name param
-				//},
-				function(images:Array<Image>) { // after all images is loaded
-					// do it sync now
-					// TODO: name param
-					var x = 0;
-					var y = 0;
-					for (image in images) {
-						//image.premultiplied = true;
-						//trace("BPP: "+image.buffer.bitsPerPixel);
-						//trace("FORMAT: "+image.buffer.format);
-						//trace("TRANSPARENT: " + image.buffer.transparent);
-						var p = textureCache.addImage(image);
-						trace( '${image.width}x${image.height}', "texture-unit:"+p.unit,"texture-slot"+p.slot);
-						buffer.addElement(new Elem(x, y, 100, 100, image.width, image.height, p.unit, p.slot));
-						x += 100;
-						if (x >= 800) {
-							x = 0;
-							y += 100;
-						}
+				function(index:Int, loaded:Int, size:Int) {
+					trace(' File number $index progress ' + Std.int(loaded / size * 100) + "%" , ' ($loaded / $size)');
+/*					progressSumA[index] = loaded;
+					progressSumB[index] = size;
+					size = 0;
+					for (x in progressSumB) {
+						if (x == 0) { size = 0; break; }
+						size += x;
 					}
-					program.setMultiTexture(textureCache.textures, "custom");
+					if (size > 0) {
+						loaded = 0;
+						for (x in progressSumA) loaded += x;
+						trace(' Total progress ' + Std.int(loaded / size * 100) + "%" , ' ($loaded / $size)');
+					}
+*/				},
+				function(index:Int, image:Image) { // after every single image is loaded
+					trace('File number $index loaded completely.');
+					var p = textureCache.addImage(image);
+					trace( '${image.width}x${image.height}', "texture-unit:"+p.unit,"texture-slot"+p.slot);
+					var x = index % 8;
+					var y = Std.int(index / 8);
+					buffer.addElement(new Elem(x*100, y*100, 100, 100, image.width, image.height, p.unit, p.slot));
+				},
+				function(images:Array<Image>) { // after all images is loaded
+					trace(' --- all images loaded ---');
 				}
 			);
 			
--- src/utils/Loader.hx
+++ src/utils/Loader.hx
@@ -22,96 +22,62 @@ class Loader
 		trace ('Loading complete "$name"');
 	}
 
-	public static inline function image( name:String, debug=false, ?onProgress:Int->Int->Void, ?onError:String->Void, onLoad:Image->Void):Void {
+	public static inline function image( name:String, debug=false, ?onProgress:Int->Int->Void, ?onError:String->Void, ?onLoad:Image->Void):Void {
 		#if html5
 		if (corsServer != "" && ~/^https?:\/\//.match(name)) name = "//"+corsServer+"/"+name;
 		#end
-
-		var future = Image.loadFromFile(name);
-		
+		var future = Image.loadFromFile(name);		
 		if (debug) {
 			trace('Start loading image "$name"');
 			future.onProgress( function(a:Int, b:Int) onProgressDebug(a, b, name) );
 			future.onError( onErrorDebug );
 			future.onComplete( function(image:Image) onCompleteDebug(name) );
-		}
-		
+		}		
 		if (onProgress != null) future.onProgress( onProgress );
 		if (onError != null) future.onError( onError );
-		future.onComplete( onLoad );		
+		if (onLoad != null) future.onComplete( onLoad );		
 	}
 	
-	public static inline function imageArray( names:Array<String>, debug=false, ?onProgress:Int->Int->Void, ?onError:String->Void, ?onLoad:Image->Void, onLoadAll:Array<Image>->Void):Void {
+	public static inline function imageArray( names:Array<String>, debug=false, ?onProgress:Int->Int->Int->Void, ?onError:Int->String->Void, ?onLoad:Int->Image->Void, ?onLoadAll:Array<Image>->Void):Void {
 		var images = new Array<Image>();
 		var loaded:Int = names.length;
-		#if !html5
-		var progressSumA:Array<Int> = [for(i in 0...names.length) 0];
-		var progressSumB:Array<Int> = [for (i in 0...names.length) 0];
-		#end
 		for (i in 0...names.length) {
 			image( names[i], debug, 
-				#if !html5
-				function (a:Int, b:Int) {
-					progressSumA[i] = a;
-					progressSumB[i] = b;
-					var bSum = 0;
-					for (x in progressSumB) {
-						if (x == 0) { bSum = 0; break; }
-						bSum += x;
-					}
-					if (bSum > 0) {
-						var aSum = 0;
-						for (x in progressSumA) aSum += x;
-						onProgress(aSum, bSum);
-					}
-				},
-				#else
-				onProgress,
-				#end
-				onError,
-				function(image:Image) {
+				(onProgress == null) ? null : function (a:Int, b:Int) onProgress(i, a, b),
+				(onError == null) ? null : function(msg:String) onError(i, msg),
+				(onLoadAll == null) ? null : function(image:Image) {
 					images[i] = image;
-					#if html5
-					onProgress(names.length-loaded+1, names.length);
-					#end
-					if (onLoad != null) onLoad(image);
-					if (--loaded == 0) {
-						onLoadAll(images);
-						images = null;
-					}
+					if (onLoad != null) onLoad(i, image);
+					if (--loaded == 0) onLoadAll(images);
 				}
 			);
 		}
 	}
 	
-	public static inline function bytes( name:String, debug=false, ?onProgress:Int->Int->Void, ?onError:String->Void, onLoad:Bytes->Void):Void {
-		var future = Bytes.loadFromFile(name);
-		
+	public static inline function bytes( name:String, debug=false, ?onProgress:Int->Int->Void, ?onError:String->Void, ?onLoad:Bytes->Void):Void {
+		var future = Bytes.loadFromFile(name);		
 		if (debug) {
 			trace('Start loading bytes "$name"');
 			future.onProgress( function(a:Int, b:Int) onProgressDebug(a, b, name) );
 			future.onError( onErrorDebug );
 			future.onComplete( function(bytes:Bytes) onCompleteDebug(name) );
-		}
-		
+		}		
 		if (onProgress != null) future.onProgress( onProgress );
 		if (onError != null) future.onError( onError );
-		future.onComplete( onLoad );		
+		if (onLoad != null) future.onComplete( onLoad );		
 	}
 		
-	public static inline function json( name:String, debug=false, ?onProgress:Int->Int->Void, ?onError:String->Void, onLoad:Json->Void):Void {
-		var future = Bytes.loadFromFile(name);
-		
+	public static inline function json( name:String, debug=false, ?onProgress:Int->Int->Void, ?onError:String->Void, ?onLoad:Json->Void):Void {
+		var future = Bytes.loadFromFile(name);		
 		if (debug) {
 			trace('Start loading json "$name"');
 			future.onProgress( function(a:Int, b:Int) onProgressDebug(a, b, name) );
 			future.onError( onErrorDebug );
 			future.onComplete( function(bytes:Bytes) onCompleteDebug(name) );
-		}
-		
+		}		
 		if (onProgress != null) future.onProgress( onProgress );
 		if (onError != null) future.onError( onError );
-		future.onComplete( function(bytes:Bytes) {
+		if (onLoad != null) future.onComplete( function(bytes:Bytes) {
 			
 			var rComments = new EReg("//.*?$", "gm");
 			var rEmptylines:EReg = new EReg("([ \t]*\r?\n)+", "g");
