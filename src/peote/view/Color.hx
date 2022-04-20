5788de93ec37570fbfe48a66d8301a4244a1f64b
Sylvio Sell <info@maitag.de>
2022-04-20 07:52:12 +0200

    fixing Color to cast also to UInt for haxe 4.15

--- src/peote/view/Color.hx
+++ src/peote/view/Color.hx
@@ -1,7 +1,7 @@
 package peote.view;
 import peote.view.utils.Util;
 
-abstract Color(Int) from Int to Int
+abstract Color(Int) from Int to Int from UInt to UInt
 {
 	inline function new(rgba:Int) this = rgba;
 	
